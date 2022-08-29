90ec40efb1993c16aad37b5d3437f380194d6be8
tada3345 <tada3345@gmail.com>
2022-08-29 15:31:23 +0900

    initial

---
+++ src/data/FeedbackData.js
@@ -0,0 +1,29 @@
+const FeedbackData = [
+    {
+        id:1,
+        rating:10,
+        text:'lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem '
+    },
+    {
+        id:2,
+        rating:6,
+        text:'lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem '
+    },
+    {
+        id:3,
+        rating:7,
+        text:'lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem '
+    },
+    {
+        id:4,
+        rating:9,
+        text:'lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem '
+    },
+    {
+        id:5,
+        rating:8,
+        text:'lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem '
+    },
+]
+
+export default FeedbackData;
